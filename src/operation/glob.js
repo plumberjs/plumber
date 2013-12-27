5f8efcbe1d9b8aacfe0f899f095cb7c3560eec09
Sébastien Cevey <seb.cevey@guardian.co.uk>
2013-12-27 18:01:03 +0000

    Recursive glob.within(...).within(...)

--- src/operation/glob.js
+++ src/operation/glob.js
@@ -1,11 +1,21 @@
 var Resource = require('../model/resource');
 
 var q = require('q');
+var path = require('path');
 var globLib = require('glob');
 var flatten = require('flatten');
 
 var glob = q.denodeify(globLib);
 
+function identity(x){ return x; }
+
+// FIXME: native helper?
+// compose(f, g)(x) == f(g(x))
+function compose(f, g) {
+    return function() {
+        return f(g.apply(null, arguments));
+    };
+}
 
 function filenameToResource(paths) {
     return paths.map(function(path) {
@@ -13,12 +23,29 @@ function filenameToResource(paths) {
     });
 }
 
-module.exports = function(files) {
-    return function(/* TODO: resources? */) {
-        return q.
-            // needed to avoid passing more map arguments to glob
-            all(files.map(function(f){ return glob(f); })).
-            then(flatten).
-            then(filenameToResource);
+function globOperation(mapper) {
+    function op(/* files... */) {
+        var fileList = flatten([].slice.call(arguments));
+        if (mapper) {
+            fileList = fileList.map(mapper);
+        }
+        return function(/* TODO: resources? */) {
+            return q.
+                // needed to avoid passing more map arguments to glob
+                all(fileList.map(function(f){ return glob(f); })).
+                then(flatten).
+                then(filenameToResource);
+        };
     };
+
+    // recursively compose mappers
+    op.within = function(directory) {
+        return globOperation(compose(mapper, function(file) {
+            return path.join(directory, file);
+        }));
+    };
+
+    return op;
 };
+
+module.exports = globOperation(identity);
