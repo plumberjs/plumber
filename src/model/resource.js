6ad59168b82726aa984b8e1d810e6e00af86d5a4
Sébastien Cevey <seb.cevey@guardian.co.uk>
2013-12-30 16:54:11 +0000

    Model Resource filename as structure fragments

--- src/model/resource.js
+++ src/model/resource.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var extend = require('extend');
+
 var stringToPath = require('../util/string-to-path');
 
 function Resource(properties) {
@@ -7,8 +9,18 @@ function Resource(properties) {
     // TODO: record filename, by default map to same filename
     this._path = properties.path && stringToPath(properties.path);
     this._data = properties.data;
-    this._filename = properties.filename ||
+
+    // Save filename as structure fragments
+    var filename = properties.filename ||
         (properties.path && extractFilename(properties.path));
+
+    this._file_name = properties.file_name ||
+        (filename && filename.split('.').slice(0, -1).join('.'));
+    this._file_trns = properties.file_trns || [];
+    this._file_exts = properties.file_exts ||
+        (filename && filename.split('.').slice(-1));
+
+    this._transformations = [];
 
     function extractFilename(path) {
         return path.split('/').slice(-1)[0];
@@ -20,7 +32,7 @@ Resource.prototype.path = function() {
 };
 
 Resource.prototype.filename = function() {
-    return this._filename;
+    return [this._file_name].concat(this._file_trns, this._file_exts).join('.');
 };
 
 Resource.prototype.data = function() {
@@ -33,5 +45,37 @@ Resource.prototype.data = function() {
     }
     return this._data;
 };
+
+
+
+Resource.prototype.copy = function(override) {
+    var properties = {
+        // FIXME: forward path or not? (needed for rename only?)
+        path:     this._path && this._path.absolute(),
+        data:     this._data,
+        file_name: this._file_name,
+        file_trns: this._file_trns,
+        file_exts: this._file_exts,
+        transformations: this._transformations
+    };
+    return new Resource(extend(properties, override));
+};
+Resource.prototype.withTransformation = function(name, filenameComponent) {
+    var ext = filenameComponent || name;
+    return this.copy({
+        file_trns: this._file_trns.concat(ext),
+        transformations: this._transformations.concat(name)
+    });
+};
+Resource.prototype.withExtension = function(suffix) {
+    return this.copy({file_exts: this._file_exts.concat(suffix)});
+};
+Resource.prototype.replaceExtension = function(ext) {
+    return this.copy({file_exts: [ext]});
+};
+Resource.prototype.withData = function(data) {
+    return this.copy({data: data});
+};
+
 
 module.exports = Resource;
