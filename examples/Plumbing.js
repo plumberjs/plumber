8ebe7454471d9700a3b1253152cd2201adefc5ef
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-01-07 16:28:31 +0000

    Fix paths

--- examples/Plumbing.js
+++ examples/Plumbing.js
@@ -8,30 +8,30 @@ var write     = require('plumber-write');
 
 module.exports = function(pipelines) {
 
-    var sources = 'examples/**/*.js';
+    var sources = 'src/**/*.js';
     var dest = 'out';
     var writeToOut = write('out');
 
     // Pass all JS files through pipeline
-    pipelines['concat'] = [glob('examples/*.js'), uglify, concat('all'), writeToOut];
+    pipelines['concat'] = [glob(sources), uglify, concat('all'), writeToOut];
 
     // Minify all files and write to out directory
-    pipelines['uglify'] = [glob('examples/*.js'), uglify, writeToOut];
+    pipelines['uglify'] = [glob('src/*.js'), uglify, writeToOut];
 
     // Minify all files and try to write to single file -- FAILS!
-    pipelines['uglify-fail'] = [glob('examples/*.js'), uglify, write('out/singlefile.js')];
+    pipelines['uglify-fail'] = [glob('src/*.js'), uglify, write('out/singlefile.js')];
 
     // Copying is just an empty pipeline to a new file
-    pipelines['copy-one'] = [glob('examples/some.js'), write('out/some.copy.js')];
-    pipelines['copy-to-dir'] = [glob('examples/some.js'), write('out')];
+    pipelines['copy-one'] = [glob('src/some.js'), write('out/some.copy.js')];
+    pipelines['copy-to-dir'] = [glob('src/some.js'), write('out')];
 
     // Pass all JS files through pipeline
-    pipelines['rjs'] = [glob('examples/amd.js'), requirejs, write('out/amd.js')];
+    pipelines['rjs'] = [glob('src/amd.js'), requirejs, write('out/amd.js')];
 
     // Hash all files and write renamed version and mapping
-    pipelines['hash'] = [glob('examples/*.js'), hash, write('out')];
+    pipelines['hash'] = [glob('src/*.js'), hash, write('out')];
 
     // Pass LESS files to less
-    pipelines['less'] = [glob('examples/*.less'), less, write('out')];
+    pipelines['less'] = [glob('src/*.less'), less, write('out')];
 
 };
